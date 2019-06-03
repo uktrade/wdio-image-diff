b28946218230f27874a73c3b6ca31c1113d86fa3
Pippo Raimondi <filipporai@gmail.com>
2019-06-03 10:55:20 +0100

    Remove unecessary file and move class to index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-export { WdioImage } from './wdio-image'
+import assert from 'assert'
+import path from './config/config'
+import createDir from './utils'
+import imageDiff from './commands/image-diff'
+import saveScreenshot from './commands/save-screenshot'
+
+class WdioImage {
+  constructor(browser) {
+    // webdriverIO browser instance
+    this._browser = browser
+
+    // Create parent directory
+    createDir(path.parentDir)
+
+    // Create subfolders in parent directory
+    Object.values(path.dir).forEach(dir => {
+      createDir(dir)
+    })
+  }
+
+  take(testName) {
+    saveScreenshot(this._browser, testName)
+  }
+
+  validate(testName) {
+    const result = imageDiff(testName)
+    return assert(result == 0)
+  }
+}
+
+export default WdioImage
