8844a7479094ccea0ef727722d832a5bb79fb0c8
Pippo Raimondi <filipporai@gmail.com>
2020-02-11 13:24:54 +0000

    Add e2e tests

---
+++ e2e/specs/visual-test-spec.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+
+describe('Visual test', () => {
+  it('should compare entire page screenshot', async () => {
+    await browser.url('/report')
+    await browser.imageDiff.take()
+    await browser.imageDiff.validate().then((result) => {
+      assert.strictEqual(result, 0)
+    })
+  })
+
+  it('should compare element screenshot', async () => {
+    await browser.url('/report')
+    await browser.imageDiff.takeElement('h2')
+    await browser.imageDiff.validate().then((result) => {
+      assert.strictEqual(result, 0)
+    })
+  })
+})
