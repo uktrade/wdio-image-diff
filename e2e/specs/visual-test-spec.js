992358c6fe839480c67f9c812344db8c229e6aef
Pippo Raimondi <filipporai@gmail.com>
2020-02-18 17:12:10 +0000

    feat: add docker compose capability and update packages

--- e2e/specs/visual-test-spec.js
+++ e2e/specs/visual-test-spec.js
@@ -16,4 +16,17 @@ describe('Visual test', () => {
       assert.strictEqual(result, 0)
     })
   })
+
+  it('should remove element before taking screenshot', async () => {
+    browser.execute(() => {
+      const elemToRemove = document.querySelector('h2')
+      elemToRemove.remove()
+    })
+
+    await browser.url('/report')
+    await browser.imageDiff.take()
+    await browser.imageDiff.validate().then((result) => {
+      assert.strictEqual(result, 0)
+    })
+  })
 })
